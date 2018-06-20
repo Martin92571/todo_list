76589c7efa0c16951ecf93da29d4cc3592f41d83
Martin92571 <zegrizo@gmail.com>
2018-06-20 16:13:04 -0700

    Completed Display dummydata

---
+++ src/data/list.js
@@ -0,0 +1,26 @@
+export default [
+    {
+   title:'Wash Car',
+   details:'Clean outside of car',
+   id:"001"
+},{
+    title:'Wash kids',
+    details:'Clean outside of kids',
+    id:"002"
+ },
+ {
+    title:'Wash house',
+    details:'Clean outside of house',
+    id:"003"
+ },
+ {
+    title:'Wash dogs',
+    details:'Clean outside of dogs',
+    id:"004"
+ },
+ {
+    title:'Wash teeth',
+    details:'Clean inside of teeth',
+    id:"005"
+ }
+];
