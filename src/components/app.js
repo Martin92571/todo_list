8a8388fca3ffa10096eade9026899118159d8718
Martin92571 <zegrizo@gmail.com>
2018-06-20 18:03:54 -0700

    added item function

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React,{Component} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import List from './list'
+import AddItem from './add_item';
+import List from './list';
+import listData from '../data/list';
 
 
-const App = () => (
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            list:[]
+        };
+    }
+    componentDidMount(){
+        this.getListData();
+    }
+    addItem(item){
+     this.setState({
+         list:[item,...this.state.list]
+     });
+    }
+    getListData(){
+        this.setState({
+            list:listData
+        })
+    }
+    render(){
+        console.log('App',this.state);
+        return(
     <div className="container">
+       
         <h1 className="center">To Do List</h1>
-        <List/>
-
+        <AddItem add={this.addItem.bind(this)}/>
+        <List data={this.state.list}/>
+        
     </div>
-);
+    );
+    }
+}
 
 export default App;
